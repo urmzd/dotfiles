bcf5423cd58882c52ccab2592ecd553033ada46f
Urmzd <urmzds@gmail.com>
2021-09-21 23:02:12 -0400

    Ensure plugins manage themselves

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,10 @@ local opt = vim.opt
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer/nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({'git' 'clone' '-depth', '1' 'https://github.com/wbthomason/packer.nvim', install_path})
+	fn.system({'git', 'clone', '-depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 	cmd 'packadd packer.nvim'
 end
+
+require('plugins')
+
+
---
+++ nvim/plugins.lua
@@ -0,0 +1,4 @@
+return require('packer').startup(function() 
+	use {'webthomason/packer.nvim', opt=true}
+
+end)
