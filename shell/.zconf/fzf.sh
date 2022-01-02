9b02437b4c00fb4fac4df392bc9413a4351c10c6
Urmzd <urmzds@gmail.com>
2022-01-01 20:31:27 -0500

    impl - include fdfind

--- shell/.zconf/fzf.sh
+++ shell/.zconf/fzf.sh
@@ -4,5 +4,5 @@ queryinstallpackage "ripgrep" 1
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # FZF Settings.
-export FZF_DEFAULT_COMMAND="fd --type f"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+#export FZF_DEFAULT_COMMAND="fdfind --type f"
+#export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
