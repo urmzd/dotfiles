de1475ee6650746ead8737d8e6104095e9d32525
Urmzd <urmzds@gmail.com>
2022-01-30 17:05:24 -0500

    impl debugger support

--- nvim/init.lua
+++ nvim/init.lua
@@ -95,6 +95,12 @@ require('telescope').load_extension('dap')
 local dap_install = require('dap-install')
 local dbg_list = require('dap-install.api.debuggers').get_installed_debuggers()
 
+--[[
+   [dap_install.setup({
+   [        installation_path = vim.fn.stdpath("data") .. "/dapinstall/"
+   [})
+   ]]
+
 for _, debugger in ipairs(dbg_list) do dap_install.config(debugger) end
 
 -- Use an on_attach function to only map the following keys
