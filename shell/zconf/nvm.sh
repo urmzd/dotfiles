4393500b20e29bf93b659eb00a24310ae376dd19
Urmzd <urmzds@gmail.com>
2022-03-24 20:25:15 -0400

    Remove old nvm code.

--- shell/zconf/nvm.sh
+++ shell/zconf/nvm.sh
@@ -1,5 +1,4 @@
 NVM_DIR="$HOME/.nvm"
-PROFILE="$ROOT/zconf/nvm-results.sh"
 NVM_COMPLETION="$HOME/.nvm/bash_completion"
 
 if [[ ! -d "$NVM_DIR" ]]
@@ -7,5 +6,4 @@ then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 fi
 
-source "$PROFILE"
 source "$NVM_COMPLETION"
