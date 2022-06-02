492c1d6bd5bcf184495039ceb20f2fe1e179d81a
Urmzd <urmzds@gmail.com>
2022-06-02 16:42:33 -0400

    Refactor EFM to consist of modular configs.

--- nvim/language-servers/efm/init.lua
+++ nvim/language-servers/efm/init.lua
@@ -7,29 +7,31 @@ local lume = require("lume")
 local lsp_servers = require("nvim.utils.path").lsp_servers_dir
 
 local M = {}
-     local M.default_settings = {
-         yaml = {yamllint},
-         json = {prettier},
-         jsonc = {prettier},
-         lua = {luafmt},
-         javascript = {eslint, prettier},
-         javascriptreact = {eslint, prettier},
-         typescript = {eslint, prettier},
-         typescriptreact = {eslint, prettier},
-         markdown = {markdownlint, prettier}
-     }
+local default_settings = {
+    yaml = { yamllint },
+    json = { prettier },
+    jsonc = { prettier },
+    lua = { luafmt },
+    javascript = { eslint, prettier },
+    javascriptreact = { eslint, prettier },
+    typescript = { eslint, prettier },
+    typescriptreact = { eslint, prettier },
+    markdown = { markdownlint, prettier }
+}
 
-local efmls = lsp_servers .. "efmls"
-           local overrides = {
-cmd = {efmls, "-logfile", "/tmp/efm.log"},
-init_options = {documentFormatting = true, codeAction = true},
-filetypes = vim.tbl_keys(efm_settings),
-settings = {languages = efm_settings}
+M.default_settings = default_settings
+
+local efmls = lsp_servers .. "efm/efm-langserver"
+
+local overrides = {
+    cmd = { efmls, "-logfile", "/tmp/efm.log" },
+    init_options = { documentFormatting = true, codeAction = true },
+    filetypes = vim.tbl_keys(efm_settings),
+    settings = { languages = efm_settings }
 }
 
 function M.setup(config, opts)
-   config.efm.setup(lume.merge(opts, overrides))
+    config.efm.setup(lume.merge(opts, overrides))
 end
 
 return M
-
--- nvim/language-servers/python.lua
+++ nvim/language-servers/python.lua
@@ -3,3 +3,5 @@ local M = {}
 function M.setup(config, opts)
    config.pyright.setup(opts)
 end
+
+return M
