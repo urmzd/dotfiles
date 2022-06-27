fc868c9732eeb938741e3514d7d46696db8be9c2
Urmzd Mukhammadnaim <urmzd@dal.ca>
2022-06-27 19:08:03 -0400

    Add r-dev-tools to enable installation of libraries.

--- install/src/R.sh
+++ install/src/R.sh
@@ -6,3 +6,4 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 sudo apt install --no-install-recommends -y r-base
 sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+ -y
+sudo apt-get install r-base-dev
