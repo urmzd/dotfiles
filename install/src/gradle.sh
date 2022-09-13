41dd35d3d370716bb995adbd37b52e912d26ca53
Urmzd <urmzd@dal.ca>
2022-09-13 15:48:56 -0400

    Add gradle + update rust.

---
+++ install/src/gradle.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/gradle/gradle.git
+git clone https://github.com/gradle/gradle-completion ~/.oh-my-zsh/plugins/gradle-completion
+
+# TODO - finish installation steps.
+# cd gradle
+# ./gradlew :distributions-full:binDistributionZip
+# cp subprojects/distributions-full/build/distributions/
--- zsh/src/zconf/rust.sh
+++ zsh/src/zconf/rust.sh
@@ -4,21 +4,5 @@ CARGO_PATH="$HOME/.cargo/bin"
 
 export PATH="$PATH:$HOME/.cargo/bin"
 
-COMPLETIONS_FOLDER="$HOME/.oh-my-zsh/completions"
-RUSTUP_COMPLETION="$COMPLETIONS_FOLDER/_rustup" 
-CARGO_COMPLETION="$COMPLETIONS_FOLDER/_cargo"
-
-if [[ ! -d $COMPLETIONS_FOLDER ]]
-then
-  mkdir -p "$COMPLETIONS_FOLDER"
-fi
-
-if [[ ! -f "$RUSTUP_COMPLETION" ]] 
-then
-  rustup completions zsh > "$RUSTUP_COMPLETION" 
-fi
-
-if [[ ! -f "$CARGO_COMPLETION" ]]
-then
-  rustup completions zsh cargo > "$CARGO_COMPLETION"
-fi
+rustup completions zsh > ~/.oh-my-zsh/completions/_rustup
+rustup completions zsh cargo > ~/.oh-my-zsh/completions/_cargo
