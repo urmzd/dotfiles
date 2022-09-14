9f05d65cf104db464406dfbcd2494bf5390a1b23
Urmzd <urmzd@dal.ca>
2022-09-14 15:25:29 -0400

    Support plugins.

--- install/src/essentials.sh
+++ install/src/essentials.sh
@@ -5,3 +5,4 @@ sudo apt-get install software-properties-common -y
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
 sudo apt-get install git -y
 sudo apt-get install build-essential -y
+sudo apt-get install zip -y
--- install/src/ripgrep.sh
+++ install/src/ripgrep.sh
@@ -1,5 +1,7 @@
-wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
-tar -xzf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
-sudo mv ripgrep-13.0.0-x86_64-unknown-linux-musl /usr/local/bin/ripgrep
-rm -rf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
-rm -rf ripgrep-13.0.0-x86_64-unknown-linux-musl
+#!/usr/bin/env bash
+
+git clone https://github.com/BurntSushi/ripgrep.git
+cd ripgrep
+cargo install --path .
+cd ..
+rm -rf ripgrep
