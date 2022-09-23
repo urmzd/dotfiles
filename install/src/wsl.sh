0e978ac330cf034c9092837072a0eafb32ca856d
Urmzd <urmzd@dal.ca>
2022-09-23 11:24:27 -0400

    Remove redundant files.

--- install/src/wsl.sh
+++ install/src/wsl.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env zsh
 
-git clone gh:equalsraf/win32yank.git
+git clone git@github.com:equalsraf/win32yank.git
 cd win32yank
 git checkout e229190
 rustup target add x86_64-pc-windows-gnu
-cargo build --release
-
-
+cargo build --release --target x86_64-pc-windows-gnu
+sudo cp target/x86_64-pc-windows-gnu/release/win32yank.exe /usr/local/bin
