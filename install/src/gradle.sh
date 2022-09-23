35c16a72b94c2dc883f5360dbd7776e91c7acbea
Urmzd <urmzd@dal.ca>
2022-09-23 09:46:06 -0400

    Remove unused scripts.

--- install/src/gradle.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/gradle/gradle-completion ~/.oh-my-zsh/plugins/gradle-completion
-sdk install gradle 6.1
-
-# TODO - finish installation steps.
-# cd gradle
-# ./gradlew :distributions-full:binDistributionZip
-# cp subprojects/distributions-full/build/distributions/
