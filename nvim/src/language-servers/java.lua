d82dab24c6cb6380f269774cfcd1eecd07619477
Urmzd <urmzd@dal.ca>
2022-09-23 13:21:47 -0400

    Fix Java.

--- nvim/src/language-servers/java.lua
+++ nvim/src/language-servers/java.lua
@@ -2,30 +2,31 @@ local root_dir = require("utils.path").root_dir;
 
 local M = {}
 
-local function get_java_cmd()
-   local executable = "java"
-   local jar = vim.fn.expand(table.concat({
-      root_dir, "plugins", "org.eclipse.equinox.launcher_*.jar"
-   }, "/"))
-   return {
-      executable, "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-      "-Dosgi.bundles.defaultStartLevel=4",
-      "-Declipse.product=org.eclipse.jdt.ls.core.product",
-      "-Dlog.protocol=true", "-Dlog.level=ALL", "-Xms1g", "-Xmx2G",
-      "-javaagent:" .. table.concat ({ root_dir, "lombok.jar" }, "/"), "-jar", jar,
-      "-configuration", table.concat ({ root_dir, "config_linux" }, "/"), "-data",
-      vim.env.WORKSPACE and vim.env.WORKSPACE or
-          table.concat ({ vim.env.HOME, "workspace" }, "/"), "--add-modules=ALL-SYSTEM",
-      "--add-opens", "java.base/java.util=ALL-UNNAMED", "--add-opens",
-      "java.base/java.lang=ALL-UNNAMED"
-   }
-end
+-- local function get_java_cmd()
+--    local executable = "java"
+--    local jar = table.concat({
+--       root_dir, "plugins", "org.eclipse.equinox.launcher_*.jar"
+--    }, "/")
+--    return {
+--       executable, "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+--       "-Dosgi.bundles.defaultStartLevel=4",
+--       "-Declipse.product=org.eclipse.jdt.ls.core.product",
+--       "-Dlog.protocol=true", "-Dlog.level=ALL", "-Xms1g", "-Xmx2G",
+--       "-javaagent:" .. table.concat({ root_dir, "lombok.jar" }, "/"), "-jar", jar,
+--       "-configuration", table.concat({ root_dir, "config_linux" }, "/"), "-data",
+--       vim.env.WORKSPACE and vim.env.WORKSPACE or
+--           table.concat({ vim.env.HOME, "workspace" }, "/"), "--add-modules=ALL-SYSTEM",
+--       "--add-opens", "java.base/java.util=ALL-UNNAMED", "--add-opens",
+--       "java.base/java.lang=ALL-UNNAMED"
+--    }
+-- end
 
 local lume = require("lume")
 
 function M.setup(config, opts)
-   config.jdtls.setup({
-      opts = lume.merge(opts, { cmd = get_java_cmd() }),
+   config.jdtls.setup(
+      lume.merge(opts, { 
+      -- cmd = get_java_cmd(), 
       settings = {
          ["java.format.settings.url"] = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
          ["java.format.settings.profile"] = "GoogleStyle",
@@ -33,8 +34,9 @@ function M.setup(config, opts)
          ["java.trace.server"] = "verbose",
          ["java.maven.downloadSources"] = true,
          ["java.import.maven.enabled"] = true
-      }
-   })
+      },
+      use_lombok_agent=true
+   }))
 
 end
 
