e7f29dd78742e7f26394865eb711f70fbe00060f
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-09-26 23:52:27 -0400

    Fix nvim setup.

--- nvim/bootstrap.sh
+++ nvim/bootstrap.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/nvim
-echo 'package.path = package.path .. ";~/dotfiles/nvim/src/?.lua"' > ~/.config/nvim/init.lua
-echo 'require("custom_nvim")' >> ~/.config/nvim/init.lua
+NVIM_CONFIG_LOCATION="$HOME/.config/nvim"
+NVIM_INIT_LOCATION="$NVIM_CONFIG_LOCATION/init.lua"
+
+echo "$NVIM_CONFIG_LOCATION"
+echo "$NVIM_INIT_LOCATION"
+
+NVIM_PACKAGE_PATH="$HOME/dotfiles/nvim/src/?.lua"
+
+mkdir -p "$NVIM_CONFIG_LOCATION"
+echo 'package.path = package.path .. ";'"$NVIM_PACKAGE_PATH"'"' > "$NVIM_INIT_LOCATION"
+echo 'require("custom_nvim")' >> "$NVIM_INIT_LOCATION"
