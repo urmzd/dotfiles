addcd18b0897832713830167c1ea732858cb6317
Urmzd <urmzd@dal.ca>
2022-09-28 17:13:04 -0400

    Update java lsp.

--- nvim/src/language-servers/java.lua
+++ nvim/src/language-servers/java.lua
@@ -1,34 +1,21 @@
-local root_dir = require("utils.path").root_dir;
-
 local M = {}
-
--- local function get_java_cmd()
---    local executable = "java"
---    local jar = table.concat({
---       root_dir, "plugins", "org.eclipse.equinox.launcher_*.jar"
---    }, "/")
---    return {
---       executable, "-Declipse.application=org.eclipse.jdt.ls.core.id1",
---       "-Dosgi.bundles.defaultStartLevel=4",
---       "-Declipse.product=org.eclipse.jdt.ls.core.product",
---       "-Dlog.protocol=true", "-Dlog.level=ALL", "-Xms1g", "-Xmx2G",
---       "-javaagent:" .. table.concat({ root_dir, "lombok.jar" }, "/"), "-jar", jar,
---       "-configuration", table.concat({ root_dir, "config_linux" }, "/"), "-data",
---       vim.env.WORKSPACE and vim.env.WORKSPACE or
---           table.concat({ vim.env.HOME, "workspace" }, "/"), "--add-modules=ALL-SYSTEM",
---       "--add-opens", "java.base/java.util=ALL-UNNAMED", "--add-opens",
---       "java.base/java.lang=ALL-UNNAMED"
---    }
--- end
 
 local lume = require("lume")
 
 function M.setup(config, opts)
-   config.jdtls.setup(
-      lume.merge(opts, {
+   local new_config = lume.merge(opts, {
          -- cmd = get_java_cmd(),
          settings = {
             java = {
+               configuration = {
+                  runtimes = {
+                     {
+                        name = "JavaSE-11",
+                        path = "~/.sdkman/candidate/java/11.0.10-open",
+                        default = true
+                     }
+                  }
+               },
                format = {
                   settings = {
                      url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
@@ -50,8 +37,11 @@ function M.setup(config, opts)
             }
          },
          use_lombok_agent = true
-      }))
+      })
 
+   
+
+   config.jdtls.setup(new_config)
 end
 
 return M
