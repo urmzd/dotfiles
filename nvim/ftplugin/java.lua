8d57b0c13605150cbd902a6f043dc24ede928e83
Urmzd <urmzd@dal.ca>
2022-11-14 14:03:40 -0500

    fix: use 17 as the default

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -4,11 +4,14 @@ local lsp_version = "1.6.400.v20210924-0641"
 local lsp_path = path.concat({jdtls_path, "plugins", "org.eclipse.equinox.launcher_"..lsp_version.. ".jar"})
 local config_path = path.concat({jdtls_path, "config_linux"})
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = '~/.workspace/' .. project_name
+local workspace_dir = '/home/urmzd/.workspace/' .. project_name
 local lombok_jar = path.concat({jdtls_path, "lombok.jar"})
+local java_path = "/home/urmzd/.sdkman/candidates/java/17.0.4-tem/bin/java"
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local cmd = {
-   'java',
+   java_path,
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
     '-Declipse.product=org.eclipse.jdt.ls.core.product',
@@ -24,10 +27,12 @@ local cmd = {
     "-data", workspace_dir
 }
 
-print(table.concat(cmd, " \\\n"))
+-- debug:
+-- print(table.concat(cmd, " \\\n"))
 
 local new_config = {
       cmd = cmd,
+      capabilities = capabilities,
       settings = {
          java = {
             configuration = {
