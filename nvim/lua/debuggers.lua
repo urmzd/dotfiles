afb70ed77a2198a5a2eaa3c8c3903fe4d60d1e1d
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-11-24 23:54:03 -0500

    feat: debugger support

--- nvim/lua/debuggers.lua
+++ nvim/lua/debuggers.lua
@@ -1,6 +1,25 @@
-require("nvim-dap-virtual-text").setup()
-require("dapui").setup()
+local dap, dap_ui = require("dap"), require("dapui")
+
+require("nvim-dap-virtual-text").setup {
+  commented = true
+}
+
+dap_ui.setup()
 
 local dap_python = require("dap-python")
+
 dap_python.setup("/home/urmzd/.pyenv/shims/python")
 dap_python.test_runner = "pytest"
+
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dap_ui.open()
+end
+
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dap_ui.close()
+end
+
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dap_ui.close()
+end
