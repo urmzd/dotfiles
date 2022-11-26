73879dcf0150f40b5f6bb0d2a5168a8fafb825bf
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-11-26 18:08:05 -0500

    fix: nerd tree config name

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -3,52 +3,51 @@ local M = {}
 -- -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local diagnostic_opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, diagnostic_opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, diagnostic_opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, diagnostic_opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, diagnostic_opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, diagnostic_opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, diagnostic_opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, diagnostic_opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, diagnostic_opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
-  
+  vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+
   if client.name ~= "null-ls" then
-       client.server_capabilities.document_formatting = false
-       client.server_capabilities.document_range_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.document_range_formatting = false
   end
-  
 end
 
 -- LSP set up.
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local opts = {
   on_attach = on_attach,
   flags = { debounce_text_changes = 150 },
-  capabilities = capabilities
+  capabilities = capabilities,
 }
 
 M.opts = opts
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,7 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
   sync_root_with_cwd = true,
   respect_buf_cwd = true,
-  update_focused_files = {
+  update_focused_file = {
     enable = true,
     update_root = true,
   },
