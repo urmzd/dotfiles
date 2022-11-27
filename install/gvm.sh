569c3299d0a73ea8c52184d16e11309371592c30
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-11-27 01:57:39 -0500

    feat: support semver

--- install/gvm.sh
+++ install/gvm.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 
+sudo apt-get install curl git mercurial make binutils bison gcc build-essential
 zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
---
+++ install/semverbot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+go install github.com/restechnica/semverbot/cmd/sbot@v1.0.0
