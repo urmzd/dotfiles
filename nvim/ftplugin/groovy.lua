9e91d68991b77ac1b1af8b8b1ac190e2564203b8
Urmzd <urmzd@dal.ca>
2022-12-07 13:51:54 -0500

    feat: support gradle

--- nvim/ftplugin/groovy.lua
+++ nvim/ftplugin/groovy.lua
@@ -1,5 +1,22 @@
 local setup = require("lsp_setup")
 local config = require("lspconfig")
+local lume = require("lume")
 local opts = setup.opts
 
-config.groovyls.setup(opts)
+local path = require("mason-core.path")
+
+local groovyls_path = path.concat({
+  vim.fn.stdpath("data"),
+  "mason",
+  "packages",
+  "groovy-language-server",
+  "build",
+  "libs",
+  "groovy-language-server-all.jar",
+})
+
+print(groovyls_path)
+
+local cmd = { "java", "-jar", groovyls_path }
+
+config.groovyls.setup(lume.merge(opts, { cmd = cmd }))
