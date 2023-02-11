1e7dec31f396384830e6570589126d287d63455f
Urmzd Mukhammadnaim <urmzds@gmail.com>
2023-02-11 14:14:18 -0400

    fix: automate docker desktop installation

--- install/docker-desktop.sh
+++ install/docker-desktop.sh
@@ -2,7 +2,6 @@
 
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.14.1-amd64.deb -O docker-desktop.deb
 
-sudo apt-get update
-sudo apt-get install ./docker-desktop.deb
+sudo apt-get update && apt-get install ./docker-desktop.deb -y
 
 rm -rf ./docker-desktop.deb
