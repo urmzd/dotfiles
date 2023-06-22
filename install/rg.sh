f69375ae6a4e779d440c85aa62ea0ff0bf7d1de1
Urmzd <urmzd@dal.ca>
2023-06-22 12:32:42 -0400

    feat: custom package manager

--- install/rg.sh
+++ install/rg.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 cd /tmp
+
 rm -rf ripgrep
+
 git clone https://github.com/BurntSushi/ripgrep.git
+
 cd ripgrep
+
 cargo install --path .
+
+cp complete/_rg ~/.oh-my-zsh/custom/completions/_rg
