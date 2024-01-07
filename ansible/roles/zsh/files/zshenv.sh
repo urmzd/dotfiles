ccae9d82e525c9527377e742686d0d77f4ddaabe
Urmzd <urmzd@dal.ca>
2024-01-06 22:27:15 -0500

    feat: add package managers

--- ansible/roles/zsh/files/zshenv.sh
+++ ansible/roles/zsh/files/zshenv.sh
@@ -3,6 +3,6 @@
 fpath=( /home/urmzd/.local/lib/python3.10/site-packages/argcomplete/bash_completion.d "${fpath[@]}" )
 # End added by argcomplete
 
-# Extend PATH
-# replace /home/urmzd with /home/{{ ansible_user }}
+export PYENV_ROOT="$HOME/.pyenv"
 export PATH="/home/urmzd/.local/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
