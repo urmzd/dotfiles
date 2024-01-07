f43a8759501395d2ab2f4fbdef8355e0a505fdeb
Urmzd <urmzd@dal.ca>
2024-01-07 13:45:47 -0500

    fix: start to syslink

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Dotfiles
 
+An automated set up for Debian-based operating systems.
 
-```
+```bash
+	# Step 1. Install ansible.
 	pip install ansible argcomplete
-
+	# Step 2. Clean all files (if existing).
+	rm -rf ~/.zshrc ~/.zshenv ~/.nvm ~/.gvm ~/.pyenv
+	# Step 3. Execute the playbook.
+	ansible-playbook ansible/playbook --ask-became-pass --user=urmzd
 ```
--- ansible/roles/zsh/files/zshrc.sh
+++ ansible/roles/zsh/files/zshrc.sh
@@ -99,3 +99,12 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+
+[[ -s "/home/urmzd/.gvm/scripts/gvm" ]] && source "/home/urmzd/.gvm/scripts/gvm"
