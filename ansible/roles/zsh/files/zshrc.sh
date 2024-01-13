9b8bcca4a74377909351e1fb212b4ca7df1dc9ff
Urmzd <urmzd@dal.ca>
2024-01-12 21:31:12 -0500

    fix: add nvim alias

--- ansible/roles/zsh/files/zshrc.sh
+++ ansible/roles/zsh/files/zshrc.sh
@@ -15,5 +15,8 @@ source $ZSH/oh-my-zsh.sh
 
 export EDITOR='nvim'
 
+alias vi="nvim"
+alias vim="nvim"
+
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
