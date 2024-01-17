16fc6216407a4da6d973178877fa201ea9c156c9
Urmzd <urmzd@dal.ca>
2024-01-17 17:03:52 -0500

    fix: default to dark theme

--- ansible/roles/nvim/files/init.lua
+++ ansible/roles/nvim/files/init.lua
@@ -156,14 +156,14 @@ require("lazy").setup({
 			--local colorscheme = "gruvbox"
 			local colorscheme = "oxocarbon"
 			-- light | dark
-			local style = "light"
+			local style = "dark"
 
 			vim.opt.termguicolors = true
 			vim.opt.background = style
 
 			vim.cmd.colorscheme(colorscheme)
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	},
 	{
