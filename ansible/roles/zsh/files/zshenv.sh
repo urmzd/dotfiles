9915e601df249b092f970f778785a67b27828dc0
Urmzd <urmzd@dal.ca>
2024-05-11 22:40:23 -0400

    impl: describe win32yank steps

--- ansible/roles/zsh/files/zshenv.sh
+++ ansible/roles/zsh/files/zshenv.sh
@@ -2,12 +2,15 @@
 fpath=( /home/urmzd/.local/lib/python3.10/site-packages/argcomplete/bash_completion.d "${fpath[@]}" )
 # End added by argcomplete
 
-VSCODE_PATH="/mnt/c/Users/urmzd/AppData/Local/Programs/Microsoft VS Code/bin"
+if [ -d "/mnt/c" ]; then
+  VSCODE_PATH="/mnt/c/Users/urmzd/AppData/Local/Programs/Microsoft VS Code/bin"
+  export PATH="$PATH:$VSCODE_PATH"
+  export PATH="$PATH:/mnt/c/ProgramData/win32yank"
+fi
 
 export PYENV_ROOT="$HOME/.pyenv"
 
 export PATH="/home/urmzd/.local/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
-export PATH="$PATH:$VSCODE_PATH"
 
 source "$HOME/.cargo/env"
